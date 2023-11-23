bb1b2646602dc19ac79d6cea68fa91f779ed6905
Goga Pipia <tskali0zamboni@gmail.com>
2023-11-23 10:50:50 -0500

    firstBranch

--- block14/index.js
+++ block14/index.js
@@ -1,1 +1,26 @@
-console.log("fuck git");
+const myInstruments = ["flute", "clarinet", "saxophone", "trumpet", "trombone"];
+ /*
+//remove the first elements 
+myInstruments.shift();
+
+//removes the last element
+myInstruments.pop();
+
+//add the element in the first index
+myInstruments.unshift("kazu");
+
+// add new in  the last index
+myInstruments.push("ori");
+*/ 
+
+// ამატებს პირველ ეელემნტად
+myInstruments.unshift("saxophone");
+
+// დროებით აქრობს პირველ ორ ელემენტს:  saxa, flute
+const firstTwo = myInstruments.slice(2);
+
+// პრინტავს მხოლოდ ინდექს : 3 და 5 ის ელემენტებს.
+const lastTwo = myInstruments.slice(3, 5);
+console.log(lastTwo);
+
+console.log(myInstruments);
