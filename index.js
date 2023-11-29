65198eb9327e59a45d3400715878dc46e5148d5c
Goga Pipia <tskali0zamboni@gmail.com>
2023-11-28 23:16:55 -0500

    first-3-command

---
+++ index.js
@@ -0,0 +1,19 @@
+const dinner = {
+    burger: 12,
+    steak: 20,
+    soup: 8,
+    macN_cheese: 14,
+    soupN_salad: 16,
+
+}
+
+// print the table 
+// console.table(dinner);
+
+//usind "Object" - print "keys" from (dinner) 
+console.log(Object.keys(dinner));
+
+// using "object" - print " values" from (dinner)
+console.log(Object.values(dinner));
+
+console.table(Object.values(dinner));
